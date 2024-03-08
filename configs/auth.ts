44397e0924808edae58edd53e399b42443f52ade
anup <anupdineshtandale@gmail.com>
2024-03-08 19:35:03 +0530

    static website

---
+++ configs/auth.ts
@@ -0,0 +1,9 @@
+// export const DEV_PUBLIC_URL = 'http://localhost:4000/api/';
+export const DEV_PUBLIC_URL = 'https://deploy-backend-5.onrender.com/api/';  
+// export const DEV_PUBLIC_URL = 'http://3.106.238.139/api/'; 
+export const DEV_PUBLIC_CALLURL = 'http://localhost:4000/apicall/';
+export const DEV_PUBLIC_APIURL = 'http://localhost:4000/apideveloper/'
+
+export const SCORE_URL = 'http://127.0.0.1:8080/api/home';
+
+
